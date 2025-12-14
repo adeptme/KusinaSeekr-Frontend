8088ce27641c1dcdf4d0ed0c7b74b3aaa903a156
Sean Lucino <sean.lucino1@gmail.com>
2025-12-14 17:30:07 +0800

    change link

--- JS/signup.js
+++ JS/signup.js
@@ -1,5 +1,5 @@
-//const AUTH_URL = 'https://kusinaseekr-backend.onrender.com';
-const AUTH_URL = 'http://127.0.0.1:5000'
+const AUTH_URL = 'https://kusinaseekr-backend.onrender.com';
+//const AUTH_URL = 'http://127.0.0.1:5000'
 
 const signupForm = document.getElementById('signupForm');
 
